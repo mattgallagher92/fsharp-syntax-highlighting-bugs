e074b183a43513f434ee3f652a711d0ed06d9821
Matt <mjg1992@hotmail.co.uk>
2020-09-27 09:11:48 +0100

    Change code to fix syntax highlighting

--- SplitModuleAndFunctionName.fs
+++ SplitModuleAndFunctionName.fs
@@ -1,21 +1,8 @@
-﻿// FsharpSyntaxHighlightingBugs is white but should be teal.
-namespace FsharpSyntaxHighlightingBugs
+﻿namespace FsharpSyntaxHighlightingBugs
 
-// Core is white but should be teal.
 module Core =
-    // f is light blue but should be light yellow.
     let f () = ()
 
-// Dependent is white but should be teal.
 module Dependent =
-    // g is light blue but should be light yellow.
     let g () =
-        // Putting `.f` on a new line breaks syntax highlighting in the file,
-        // but compiles.
-        //
-        // When module and function names are long, and there is a bit more
-        // nesting, splitting like this can be useful to keep lines short.
-        //
-        // Replacing the next two lines with `Core.f` fixes syntax highlighting.
-        Core
-            .f
+        Core.f
